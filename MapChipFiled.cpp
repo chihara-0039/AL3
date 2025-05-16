11a8ab8ae45a8071166cad719d55d2e29565554a
chihara-shigou <k024g0039@g.neec.ac.jp>
2025-05-16 14:09:45 +0900

    資料8ページ完了

---
+++ MapChipFiled.cpp
@@ -0,0 +1,1 @@
+#include "MapChipFiled.h"
---
+++ MapChipFiled.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "KamataEngine.h"
+
+class MapChipFiled {
+public:
+	// 1ブロックのサイズ
+	static inline const float kBlockWidth = 1.0f;
+	static inline const float kBlockHeight = 1.0f;
+
+	//ブロックの個数
+	static inline const uint32_t kNumBlockVirtical = 20;
+	static inline const uint32_t kNumBlockHorizontal = 100;
+
+private:
+};
