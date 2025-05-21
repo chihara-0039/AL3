54fa415f0d9be01404c1cefd1ca9625f841f3d51
chihara-shigou <k024g0039@g.neec.ac.jp>
2025-05-21 14:00:21 +0900

    資料19枚目

--- MapChipFiled.cpp
+++ MapChipFiled.cpp
@@ -1,1 +1,70 @@
+#include <fstream>
+#include <sstream>
+#include <cassert>
 #include "MapChipFiled.h"
+#include <map>
+
+//内部リンケージ
+namespace {
+
+std::map<std::string, MapChipType> mapChipTable = {
+    {"0", MapChipType::kBlank},
+    {"1", MapChipType::kBlock},
+};
+
+}
+
+// マップチップCSVを読み込む
+void MapChipField::LoadMapChipCsv(const std::string& filePath) {
+	// マップチップデータをリセット
+	ResetMapChipData();
+
+	// ファイルを開く
+	std::ifstream file(filePath);
+	assert(file.is_open());
+
+	// マップチップCSV
+	std::stringstream mapChipCsv;
+
+	// ファイルの内容を文字列ストリームにコピー
+	mapChipCsv << file.rdbuf();
+
+	// ファイルを閉じる
+	file.close();
+
+	// CSV内容をパースして mapChipData_ に反映する処理をここに追加
+	for (uint32_t i = 0; i < kNumBlockVirtical; ++i) {
+		std::string line;
+		getline(mapChipCsv, line);
+
+		// 1行分の文字列をストリームに変換して解析しやすくする
+		std::istringstream line_stream(line);
+
+		for (uint32_t j = 0; j < kNumBlockHorizontal; ++j) {
+
+			std::string word;
+			getline(line_stream, word, ',');
+
+			if (mapChipTable.contains(word)) {
+				mapChipData_.data[i][j] = mapChipTable[word];
+			}
+		}
+	}
+}
+
+// マップチップデータをリセット
+void MapChipField::ResetMapChipData() {
+
+	//  マップチップデータをリセット
+	mapChipData_.data.clear();
+	mapChipData_.data.resize(kNumBlockVirtical);
+	for (std::vector<MapChipType>& mapChipDateLine : mapChipData_.data) {
+		mapChipDateLine.resize(kNumBlockHorizontal);
+	}
+
+	
+
+
+}
+
+
--- MapChipFiled.h
+++ MapChipFiled.h
@@ -1,8 +1,22 @@
 #pragma once
 #include "KamataEngine.h"
 
-class MapChipFiled {
+struct MapChipData {
+	std::vector<std::vector<MapChipType>> data;
+};
+
+enum class MapChipType {
+	kBlank,	//空白
+	kBlock,	//ブロック
+};
+
+class MapChipField {
 public:
+
+	void ResetMapChipData();
+
+	void LoadMapChipCsv(const std::string& filePath);
+
 	// 1ブロックのサイズ
 	static inline const float kBlockWidth = 1.0f;
 	static inline const float kBlockHeight = 1.0f;
@@ -11,5 +25,27 @@ public:
 	static inline const uint32_t kNumBlockVirtical = 20;
 	static inline const uint32_t kNumBlockHorizontal = 100;
 
+	Vector3 GetMapChipPositionByIndex(uint32_t xIndex, uint32_t yIndex) {
+		return Vector3(kBlockWidth * xIndex, kBlockHeight * (kNumBlockVirtical - 1 - yIndex), 0);
+
+	}
+
 private:
+
+	//メンバ変数
+	MapChipData mapChipData_;
+
+	
+	MapChipType GetMapChiptypeByIndex(uint32_t xIndex, uint32_t yIndex) {
+		if (xIndex < 0 || kNumBlockHorizontal - 1 < xIndex) {
+			return MapChipType::kBlank;
+		}
+
+		if (yIndex < 0 || kNumBlockVirtical - 1 < yIndex) {
+			return MapChipType::kBlank;
+		}
+
+		return mapChipData_.data[yIndex][xIndex];
+	}
+	
 };
